ba0d4b5c72077128a4c204031e8584c8f4b301c9
keshavVerma11 <75388669+keshavVerma11@users.noreply.github.com>
2021-06-06 12:03:26 -0400

    Add files via upload

---
+++ ConnectFour.java
@@ -0,0 +1,60 @@
+package com.creator;
+
+import javax.swing.*;
+
+public class ConnectFour {
+    private JButton one;
+    private JButton two;
+    private JButton three;
+    private JButton four;
+    private JButton five;
+    private JButton six;
+    private JButton seven;
+    private JPanel pos00;
+    private JPanel pos10;
+    private JPanel pos20;
+    private JPanel pos30;
+    private JPanel pos40;
+    private JPanel pos50;
+    private JPanel pos01;
+    private JPanel pos11;
+    private JPanel pos21;
+    private JPanel pos31;
+    private JPanel pos41;
+    private JPanel pos51;
+    private JPanel pos02;
+    private JPanel pos12;
+    private JPanel pos22;
+    private JPanel pos32;
+    private JPanel pos42;
+    private JPanel pos52;
+    private JPanel pos03;
+    private JPanel pos13;
+    private JPanel pos23;
+    private JPanel pos33;
+    private JPanel pos43;
+    private JPanel pos53;
+    private JPanel pos04;
+    private JPanel pos14;
+    private JPanel pos24;
+    private JPanel pos34;
+    private JPanel pos44;
+    private JPanel pos54;
+    private JPanel pos05;
+    private JPanel pos15;
+    private JPanel pos25;
+    private JPanel pos35;
+    private JPanel pos45;
+    private JPanel pos55;
+    private JPanel pos06;
+    private JPanel pos16;
+    private JPanel pos26;
+    private JPanel pos36;
+    private JPanel pos46;
+    private JPanel pos56;
+    private JPanel board;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
